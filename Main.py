c50cd0ad263bbb3b2a5c93ba0692579ce0e9882c
Coderdroiid <nhyira.opare-agyemang@bloomingbuds.edu.gh>
2025-07-22 19:36:50 +0000

    Print statement

---
+++ Main.py
@@ -0,0 +1,7 @@
+#print a statement
+print("Hello world, I am learning python development")
+#print a decimal
+print(12.6)
+print(23)
+#print a blank line
+print("\n")
---
+++ Main2.py
@@ -0,0 +1,10 @@
+#create a variable to store a name
+name= "John Doe"
+print(f"Dummy name⁚ {name}")
+#variable to store age
+age=12
+#display the age
+print(f"Age⁚ {age}")
+# a variable to store weight
+weight=35.8
+print(f"Weight⁚{weight}")
